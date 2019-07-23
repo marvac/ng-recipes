fcad6212966c64d1b70d7ac10828d252979d7eec
Marc Vaccaro <marcgvaccaro@gmail.com>
2019-07-22 23:29:03 -0700

    Build out initial UI

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Garlic Butter Baked Chicken", 
+    "Sample Description", 
+    "https://www.primaverakitchen.com/wp-content/uploads/2019/03/Garlic-Butter-Baked-Chicken-Breast-Primavera-Kitchen-2.jpg"),
+    new Recipe("Something Else", 
+    "Sample Description", 
+    "https://www.primaverakitchen.com/wp-content/uploads/2019/03/Garlic-Butter-Baked-Chicken-Breast-Primavera-Kitchen-2.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
