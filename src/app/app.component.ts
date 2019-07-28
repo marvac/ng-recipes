94067f5a3ea4666e7777a6bdfda4ffa32830b4d8
Marc Vaccaro <marcgvaccaro@gmail.com>
2019-07-27 23:20:55 -0700

    Add page navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  loadedSection: string = 'recipe';
+  onNavigate(section: string) {
+    this.loadedSection = section;
+  }
 }
